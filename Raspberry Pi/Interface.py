a35d0aafc72cad9fc048240baee0f31502a8275a
Lucas Jorgenson <lucoseglucose@github.com>
2026-04-29 20:39:47 -0400

    Added pi to laptop interface file

--- Raspberry Pi/Interface.py
+++ Raspberry Pi/Interface.py
@@ -1,10 +1,14 @@
 import serial
 import time
 
-serialToInterface = serial.Serial("/dev/ttyS0", 19200, timeout=1)
+serialToInterface = serial.Serial("/dev/ttyS0", 19200, 8, "N", 1, 1)
 serialToInterface.reset_input_buffer()
 
 time.sleep(.25)
 
-while (string := input()) != "quit":
-    serialToInterface.write(string.encode())
+while True:
+    if serialToInterface.in_waiting > 0:
+        print(serialToInterface.readall().decode())
+    else:
+        string: str = input("-")
+        serialToInterface.write(string.encode())
