1ccf1a8095941db52abc263de12caec3af69e52a
nehem <nehem@NJ_LAPTOP>
2026-05-04 17:21:25 -0400

    sorry

--- Arduino/DAQcode/DAQcode.ino
+++ Arduino/DAQcode/DAQcode.ino
@@ -54,6 +54,7 @@ void loop(){
                 digitalWrite(PI1, LOW);
                 digitalWrite(PI2, LOW);
                 digitalWrite(PI3, LOW);
+                analogWrite(A0, 0);
             }
             if (str[1]== '2'){
                 digitalWrite(PI0, HIGH);
@@ -90,6 +91,7 @@ void loop(){
                 digitalWrite(PI1, HIGH);
                 digitalWrite(PI2, HIGH);
                 digitalWrite(PI3, LOW);
+                analogWrite(A0, 0);
             }
             if (str[1] == '8'){
                 digitalWrite(PI0, HIGH);
@@ -99,7 +101,22 @@ void loop(){
             }
         }
         if (str[0] == 'R'){
+            if (str[1] =='1'){
+                analogWrite(A0, 36);
+            }
+            if (str[1] == 'D'){
+                analogWrite(A0, 61);
+            }
+            if (str[1] == '3'){
+                analogWrite(A0, 87);
+            }
+            if( str[1] == 'C'){
+                analogWrite(A0, 112);
+            }
+
+            /*
             for (int l = 1; l <= 8; l++){
+                
                 if(str[l] == '0'){
                     analogWrite(A0, 0);
                     delay(n);
@@ -165,6 +182,7 @@ void loop(){
                     delay(n);
                 }
             }
+            */
         }
         if (str[0] == 'F'){
             String F = str.substring(1);
@@ -208,8 +226,8 @@ void loop(){
                 digitalWrite(PI3, HIGH);
             }
         }
+        
     }
-
     if (Serial.available()){
         serialFromPi.print(Serial.readString());
         if (estop == HIGH){
